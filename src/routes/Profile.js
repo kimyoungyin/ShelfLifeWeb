a384cece98bdc189e5b731c9f57e56d584a7f500
kimyoungyin <mafa1234@naver.com>
2021-05-14 23:20:34 +0900

    프로필 디자인/가이드 추가

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -44,7 +44,7 @@ const Profile = ({ userObj }) => {
           <p className="Profile-step">
             1) 진열가능 <br />
             2) 진열가능 제품 등록 <br />
-            3) 오른쪽 버튼 클릭
+            3) "+" 버튼 클릭
           </p>
           <p className="Profile-explain">
             같은 이름의 진열가능 제품은 다시 등록할 수 없습니다.
@@ -66,9 +66,9 @@ const Profile = ({ userObj }) => {
           <h3 className="Profile-question">3. 진열가능한 제품을 수정하기</h3>
           <p className="Profile-step">
             1) 진열가능 <br />
-            2) 수정하기 버튼 클릭 <br />
+            2) "수정하기" 버튼 클릭 <br />
             3) 수정하고자 하는 제품의 변경(제거) 버튼 클릭 <br />
-            4) 수정 후 수정완료 버튼 클릭
+            4) 수정 후 "수정완료" 버튼 클릭
           </p>
           <p className="Profile-explain">
             제품명 변경 혹은 제품 제거를 할 수 있습니다.
@@ -79,11 +79,11 @@ const Profile = ({ userObj }) => {
             4. 진열가능한 제품의 진열 시작하기
           </h3>
           <p className="Profile-step">
-            진열가능 <br />
-            1) 진열하기 버튼 클릭 <br />
-            2) 진열하고 싶은 제품들의 "0개" 버튼을 연속클릭(혹은 초기화)하여
+            1) 진열가능 <br />
+            2) "진열하기" 버튼 클릭 <br />
+            3) 진열하고 싶은 제품들의 "0개" 버튼을 연속클릭(혹은 초기화)하여
             진열수량 정하기 <br />
-            3) 진열 버튼 클릭으로 한번에 진열 시작!
+            4) "진열" 버튼 클릭으로 한번에 진열 시작!
           </p>
           <p className="Profile-explain">
             진열중 페이지에서 확인할 수 있습니다.
@@ -94,14 +94,34 @@ const Profile = ({ userObj }) => {
             5. 진열중인 제품 제거(판매 혹은 폐기)하기
           </h3>
           <p className="Profile-step">
-            진열중 <br />
-            1) 제거하려는 제품 클릭 <br />
-            2) 제품의 개수(기본값은 진열수량)를 입력하고 "개 제거" 클릭
+            1) 진열중 <br />
+            2) 제거하려는 제품 클릭 <br />
+            3) 제품의 개수(기본값은 진열수량)를 입력하고 "개 제거" 클릭
             <br />
           </p>
           <p className="Profile-explain">
             진열중이던 제품이 판매되었거나 시간이 너무 오래되었을 경우
             제거합니다.
+          </p>
+        </article>
+        <article>
+          <h3 className="Profile-question">6. 로그아웃</h3>
+          <p className="Profile-step">
+            1) 내정보 <br />
+            2) "로그아웃" 버튼 클릭 <br />
+          </p>
+          <p className="Profile-explain">
+            본인 명의의 계정은 한 개만 생성 가능합니다.
+          </p>
+        </article>
+        <article>
+          <h3 className="Profile-question">6. 매장코드 변경</h3>
+          <p className="Profile-step">
+            1) 내정보 <br />
+            2) "다른 매장으로 접속" 버튼 클릭 <br />
+          </p>
+          <p className="Profile-explain">
+            본인 명의의 계정은 한 개만 생성 가능합니다.
           </p>
         </article>
       </article>
