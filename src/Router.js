40b454d598db48eea7900733c822de6d78f36af0
kimyoungyin <yin199859@gmail.com>
2021-12-12 23:59:56 +0900

    fix: router redirect로 잘못된 Url 접근에 대응

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import {
+    BrowserRouter as Router,
+    Redirect,
+    Route,
+    Switch,
+} from "react-router-dom";
 import Profile from "routes/Profile";
 import Auth from "./routes/Auth";
 import Navigation from "./components/Navigation";
@@ -8,33 +13,43 @@ import OnSale from "routes/OnSale";
 import MenuBar from "./components/MenuBar";
 import "./css/Router.css";
 import { useSelector } from "react-redux";
+import Register from "routes/Register";
+import Confirm from "components/Confirm";
 
 const AppRouter = () => {
     const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+    const storeCode = useSelector((state) => state.products.storeCode);
+    const deletingProduct = useSelector(
+        (state) => state.products.deletingProductObj
+    );
+
     return (
         <Router>
             <MenuBar />
             <div className="Router-switch">
-                {isLoggedIn ? (
-                    <Switch>
-                        <Route exact path="/">
-                            <Store />
-                        </Route>
-                        <Route path="/OnSale">
-                            <OnSale />
-                        </Route>
-                        <Route exact path="/profile">
-                            <Profile />
-                        </Route>
-                    </Switch>
-                ) : (
-                    <Switch>
-                        <Route exact path="/">
-                            <Auth />
-                        </Route>
-                    </Switch>
-                )}
+                {storeCode === null && <Redirect from="*" to="/register" />}
+                {isLoggedIn || <Redirect from="*" to="/auth" />}
+                <Switch>
+                    <Route exact path="/">
+                        <Store storeCode={storeCode} />
+                    </Route>
+                    <Route exact path="/auth">
+                        {isLoggedIn ? <Redirect to="/" /> : <Auth />}
+                    </Route>
+                    <Route path="/onSale">
+                        <OnSale />
+                    </Route>
+                    <Route exact path="/profile">
+                        <Profile />
+                    </Route>
+                    <Route exact path="/register">
+                        <Register storeCode={storeCode} />
+                    </Route>
+                </Switch>
             </div>
+            {deletingProduct && (
+                <Confirm confirmObj={deletingProduct} storeCode={storeCode} />
+            )}
             {isLoggedIn && <Navigation />}
         </Router>
     );
